907c524f206647c95bb1b60371e0ce14c8dbfef5
Ubuntu <ubuntu@ip-11-0-14-192.ap-northeast-1.compute.internal>
2024-10-22 07:12:25 +0000

    modify create kp

--- functions/aws/create_key_pair.py
+++ functions/aws/create_key_pair.py
@@ -1,13 +1,20 @@
+import os
 from utils.ec2_client import get_ec2_client
 
 def create_key_pair(key_name):
     ec2 = get_ec2_client()
     # Create the keypair
     key_pair = ec2.create_key_pair(KeyName=key_name)
-    
+
+
     # In the furture, we will save in database
     # Currently, we're saving private key into file
-    with open(f"utils/private/{key_name}.pem", "w") as key_file:
+    private_dir = "utils/private"
+    
+    if not os.path.exists(private_dir):
+        os.makedirs(private_dir)
+
+    with open(f"{private_dir}/{key_name}.pem", "w") as key_file:
         key_file.write(key_pair['KeyMaterial'])
     
     print(f"Key pair '{key_name}' created and saved as '{key_name}.pem'.")
