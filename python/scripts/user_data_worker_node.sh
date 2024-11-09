bf1206e185bb4559a5785f764e560016dc63f0e0
NguyenAnhTuan1912 <nguyenanhtuan19122002@gmail.com>
2024-11-09 19:10:24 +0700

    Update user data for worker node

--- python/scripts/user_data_worker_node.sh
+++ python/scripts/user_data_worker_node.sh
@@ -3,25 +3,26 @@
 exec > /var/log/user-data.log 2>&1
 
 # Set up working directory
-WORK_DIR=/home/root
+WORK_DIR=$HOME
 
-# stop script if it has error
-set -e 
+# Stop script if an error occurs
+set -e
 
-# Update package list
+# Update package list with sudo
 apt update -y
 
-# Install necessary packages
-apt install python3 python3-pip python3.10-venv -y
+# Install necessary packages with sudo
+apt install -y python3 python3-pip python3-venv curl
 
 # Create a directory for the virtual environment
 mkdir -p $WORK_DIR/ray_env  
 
-# Create a virtual environment
-python3 -m venv $WORK_DIR/ray_env  
-
-# Activate the virtual environment
+# Create and activate the virtual environment
+python3 -m venv $WORK_DIR/ray_env
 source $WORK_DIR/ray_env/bin/activate
+
+# Upgrade pip for compatibility
+pip install --upgrade pip
 
 # Install Ray
 pip install -U "ray" flask
@@ -30,4 +31,4 @@ pip install -U "ray" flask
 curl -o $WORK_DIR/workder.py https://raw.githubusercontent.com/Weminal-labs/walnet-backend/refs/heads/main/python/ray/workder.py
 
 # Add node into cluster Ray with the public IP address
-python3 $HOME/worker.py
+python3 $WORK_DIR/worker.py
