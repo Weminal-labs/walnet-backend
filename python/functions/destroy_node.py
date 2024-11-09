c7243a8d88ecb1256b394ea29941b13fdd0b0041
NguyenAnhTuan1912 <nguyenanhtuan19122002@gmail.com>
2024-11-09 12:57:13 +0700

    Update destroy node

--- python/functions/destroy_node.py
+++ python/functions/destroy_node.py
@@ -1,10 +1,17 @@
 from utils.ec2_client import get_ec2_client
 
+from functions.describe_nodes import describe_nodes
+
 def destroy_node(instance_ids):
     ec2 = get_ec2_client()
 
     response = ec2.terminate_instances(InstanceIds=instance_ids)
     instance = response['TerminatingInstances'][0]
+
+    instance_info = describe_nodes([instance['InstanceId']])[0]
+
+    # Delete security groups
+    ec2.delete_security_group(GroupId=instance_info['securityGroups'][0]['groupId'])
 
     return { 
         "id": instance['InstanceId'],
